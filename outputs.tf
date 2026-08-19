e6756e4a79ee440f4c268ffd4b248d1404610239
Atul Kamble <atul_kamble@hotmail.com>
2026-08-19 09:23:45 +0530

    code

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   description = "Public IP of the deployed EC2 instance"
   value       = aws_instance.http_server.public_ip
 }
+
+output "instance_public_dns" {
+  description = "Public DNS of the deployed EC2 instance"
+  value       = aws_instance.http_server.public_dns
+}
